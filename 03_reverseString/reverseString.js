f2542470193aeb99842299fcc51733a642df10be
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-07 18:22:50 -0400

    Completed exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+
+
+const reverseString = function(string) {
+    let len = string.length
+    let i;
+    let res = "";
+
+    for (i=0; i<len; i++){
+        res += string[(len - 1) - i];
+    }
+    return res;
 
 };
 
