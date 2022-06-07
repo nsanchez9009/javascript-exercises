c3a7d2b5ab7c776b6f95ce239de2c221f93ce0fa
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-07 18:06:34 -0400

    Completed excercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i;
+    let res = string;
 
+    if (num == 0){
+        return "";
+    }
+
+    else if (num < 0){
+        return "ERROR";
+    }
+
+    for (i=1; i<num; i++){
+        res += string;
+    }
+    return res;
 };
 
 // Do not edit below this line
