d28d9d316299525ee168b4675900e5e36e85fad5
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-08 12:29:55 -0400

    Completed exercise 6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,47 @@
-const leapYears = function() {
+function divide400(year){
+    if (year%400 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function divide100(year){
+    if (year%100 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function divide4(year){
+    if (year%4 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+const leapYears = function(year) {
+
+    switch (true) {
+        case !divide4(year):
+            return false;
+        case !divide100(year):
+            return true;
+        case !divide400(year):
+            return false;
+        default:
+            return true;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
