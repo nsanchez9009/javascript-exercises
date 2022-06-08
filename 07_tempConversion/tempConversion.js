59838dff27ceb163215f5598d77260a17dce618c
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-08 12:56:24 -0400

    Completed exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let res = ((temp - 32) * 5) / 9;
+  
+  if (res === 0){
+    return 0;
+  }
 
+  return Math.round(res * Math.pow(10, 1)) / Math.pow(10, 1);
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let res = ((temp * 9) / 5) + 32;
 
+  if (res === 0){
+    return 0;
+  }
+
+  return Math.round(res * Math.pow(10, 1)) / Math.pow(10, 1);
 };
 
 // Do not edit below this line
