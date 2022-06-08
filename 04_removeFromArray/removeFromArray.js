224afb4510dcf1e4b6c7d9248bde2a7248830281
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-07 20:27:10 -0400

    Completed exercise 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
+    let i; 
+    let array = arguments[0];
+    let len = arguments.length;
 
+    for (i = 1; i < len; i++){
+
+        if (array[array.indexOf(arguments[i])] !== arguments[i]){
+            continue;
+        }
+        else {
+            array.splice(array.indexOf(arguments[i]), 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
