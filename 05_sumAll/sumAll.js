61f01871a2cd2030d078817c33db57a2ec8d25ae
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-08 11:29:07 -0400

    Completed exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+
+    let big, small;
+    let sum = 0;
+
+    if (int1 < 0 || int2 < 0){
+        return "ERROR";
+    }
+
+    else if (!Number.isInteger(int1) || !Number.isInteger(int2)){
+        return "ERROR"
+    }
+
+    else if (int1 < int2){
+        small = int1;
+        big = int2;
+    }
+    else {
+        small = int2;
+        big = int1;
+    }
+
+    while (small <= big){
+        sum += small;
+        small++;
+    }
+
+    return sum;
+
 
 };
 
