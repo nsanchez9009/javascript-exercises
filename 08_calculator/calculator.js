0439be426717a7c272b61ea90e83616774279167
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-15 15:48:45 -0400

    completed 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a < b ? b - a : a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
+}
+
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const multiply = function() {
-
+const power = function(number, pow) {
+  return Math.pow(number, pow);
 };
 
-const power = function() {
-	
-};
+const factorial = function(number) {
+	if (number === 0) return 1;
 
-const factorial = function() {
-	
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
