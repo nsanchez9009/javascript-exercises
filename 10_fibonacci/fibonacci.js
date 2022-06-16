e988012c2fe1a9175f190a878db93c74a3a1e2c5
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-15 20:37:12 -0400

    Completed 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    
+    const array = [1];
+    let sum = 1;
+    let prev = 0;
 
+    for (let i = 1; i < number; i++){
+        sum += prev;
+        array[i] = sum;
+        prev = array[i - 1];
+    } 
+    return array[number - 1];
 };
 
 // Do not edit below this line
