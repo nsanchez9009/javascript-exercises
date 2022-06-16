a0530568b753cfee330dc17c18e046c8a23ca015
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-16 12:43:41 -0400

    Completed 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
-};
+    const peopleAges = people.sort(function(a, b){
+        if(!("yearOfDeath" in a)){
+            a.yearOfDeath = currentYear;
+        }
+        else if (!("yearOfDeath" in b)){
+            b.yearOfDeath = currentYear;
+        }
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        
+        return bAge - aAge
+    });
+    return peopleAges[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
