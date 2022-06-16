e7c3732f1d53ebe16561e2a339508bda6ecbf5a0
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-15 21:02:42 -0400

    completed 09.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const initString = string.replaceAll(/[^a-zA-Z]/g, "").toLowerCase();
+    const reverse = initString.split("").reverse().join("");
+    return initString === reverse ? true : false;
 };
 
 // Do not edit below this line
